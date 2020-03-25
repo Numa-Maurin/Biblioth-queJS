bdc2c31270728405d42c64044a4d6a30de8ef345
tupac-rocher <47113639+tupac-rocher@users.noreply.github.com>
2020-03-25 10:10:59 +0100

    Rectification

--- src/js/script.js
+++ src/js/script.js
@@ -12,7 +12,7 @@ let xhrLivreE = new XMLHttpRequest();
 //selection des divs a remplir
 let div_adherent = document.getElementById("listeAdherents");
 let div_LivreDispo = document.getElementById("listeLivresDisponibles");
-let div_LivreEmpruntés = document.getElementById("listeLivresEmpruntes");
+let div_LivreEmpruntes = document.getElementById("listeLivresEmpruntes");
 
 //ajout adherent
 let zonesaisie= document.getElementById("nomAdherent");
@@ -115,23 +115,23 @@ function callback_livreEmpruntes() {
 
     let tab = JSON.parse(xhrLivreE.responseText);
     let ul = document.createElement("ul");
-    div_LivreEmpruntés.innerHTML = "";
-    div_LivreEmpruntés.appendChild(ul);
+    div_LivreEmpruntes.innerHTML = "";
+    div_LivreEmpruntes.appendChild(ul);
 
     for (let i = 0; i != tab.length; i ++) {
         let li = document.createElement("li");
         li.id ="liv"+tab[i].idLivre;
         li.innerHTML =  tab[i].idLivre + "-"+ tab[i].titreLivre;
         li.setAttribute("onclick", "recupererLivre('"+tab[i].idLivre+"')");
-        div_LivreEmpruntés.children[0].appendChild(li);
+        div_LivreEmpruntes.children[0].appendChild(li);
     }
 
 }
 
 function charger_livresEmpruntes() {
     //on vide le div
-    while(div_LivreEmpruntés.children.length > 0){
-		div_LivreEmpruntés.removeChild(div_LivreEmpruntés.children[0]);
+    while(div_LivreEmpruntes.children.length > 0){
+		div_LivreEmpruntes.removeChild(div_LivreEmpruntes.children[0]);
     }
 
 	let url = 'http://localhost/JS/td7/src/php/collecteLivreEmpruntes.php';
